960ddda5e248a62a72ac2aee31230c97a4fb982c
오하늘 <nebulass@naver.com>
2022-09-22 21:02:15 +0900

    props 추가로 드래그 활성화

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Wrapper = styled.ul`
+	width: 400px;
+	background-color: #eaeaea;
+	list-style: none;
+	padding: 0;
+	margin: 0;
+`;
+
+const Item = styled.li`
+	padding: 20px 10px;
+	background-color: #007355;
+	margin-bottom: 10px;
+	color: #fff;
+`;
 
 function App() {
 	const onDragEnd = () => {};
@@ -6,15 +22,29 @@ function App() {
 		<>
 			<DragDropContext onDragEnd={onDragEnd}>
 				<Droppable droppableId='one'>
-					{() => (
-						<ul>
+					{(provided) => (
+						<Wrapper ref={provided.innerRef} {...provided.droppableProps}>
 							<Draggable draggableId='first' index={0}>
-								{() => <li>Hello</li>}
+								{(provided) => (
+									<Item
+										ref={provided.innerRef}
+										{...provided.dragHandleProps}
+										{...provided.draggableProps}>
+										<span>보라 안녕</span>
+									</Item>
+								)}
 							</Draggable>
 							<Draggable draggableId='second' index={1}>
-								{() => <li>Hi</li>}
+								{(provided) => (
+									<Item
+										ref={provided.innerRef}
+										{...provided.dragHandleProps}
+										{...provided.draggableProps}>
+										보라 바보
+									</Item>
+								)}
 							</Draggable>
-						</ul>
+						</Wrapper>
 					)}
 				</Droppable>
 			</DragDropContext>
